2c448d049725864114a02b3e62224b62b0443eda
wulianglongrd <wulianglongrd@gmail.com>
2025-07-12 13:54:35 +0800

    Added match and sub command

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use serde::Deserialize;
+use regex::Regex;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -7,9 +9,16 @@ pub struct Config {
 
 #[derive(Debug, Deserialize)]
 pub struct Task {
+    #[serde(default)]
     pub name: String,
-    pub commands: Vec<String>,
+    pub command: String,
     pub extract: Option<Vec<ExtractRule>>,
+    #[serde(rename = "okMatch")]
+    pub ok_match: Option<OkMatch>,
+    #[serde(rename = "whenOk")]
+    pub when_ok: Option<Box<Task>>,
+    #[serde(rename = "whenErr")]
+    pub when_err: Option<Box<Task>>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -18,4 +27,36 @@ pub struct ExtractRule {
 
     #[serde(rename = "as")]
     pub as_key: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct OkMatch {
+    #[serde(default)]
+    pub context: HashMap<String, Matcher>,
+    #[serde(default)]
+    pub output: Vec<Matcher>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Matcher {
+    pub exact: Option<String>,
+    pub prefix: Option<String>,
+    pub regex: Option<String>,
+}
+
+impl Matcher {
+    pub fn is_match(&self, input: &str) -> bool {
+        if let Some(ref exact) = self.exact {
+            return input == exact;
+        }
+        if let Some(ref prefix) = self.prefix {
+            return input.starts_with(prefix);
+        }
+        if let Some(ref regex) = self.regex {
+            if let Ok(reg) = Regex::new(regex) {
+                return reg.is_match(input);
+            }
+        }
+        false
+    }
 }
--- src/runner.rs
+++ src/runner.rs
@@ -4,47 +4,81 @@ use std::process::Command;
 use handlebars::Handlebars;
 use jsonpath_lib::select;
 use serde_json::Value;
-use crate::config::Config;
+use crate::config::{Config, Task};
 
 pub fn run_tasks(path: &str) -> anyhow::Result<()> {
     let content = fs::read_to_string(path)?;
     let config: Config = serde_yaml::from_str(&content)?;
 
     let mut context: HashMap<String, String> = HashMap::new();
-    let handlebars = Handlebars::new();
-
     for task in config.tasks {
-        println!("===> Running task {}", task.name);
-
-        for cmd_template in task.commands  {
-            let rendered = handlebars.render_template(&cmd_template, &context)?;
-            println!("$ {}", rendered);
-
-            let output = Command::new("sh").arg("-c").arg(&rendered).output()?;
-            if !output.status.success() {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                return Err(anyhow::anyhow!("Error running task {}: {}", task.name, stderr));
-            }
-
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("stdout: {}", stdout);
-
-            if let Some(rules) = &task.extract {
-                if let Ok(json) = serde_json::from_str::<Value>(&stdout) {
-                    for rule in rules {
-                        let results = select(&json, &rule.field)?;
-                        if let Some(val) = results.get(0) {
-                            let val_str = val.to_string().trim_matches('"').to_string();
-                            context.insert(rule.as_key.clone(), val_str.clone());
-                            println!("→ Extracted {} = {}", rule.as_key, val_str);
-                        }
-                    }
-                } else {
-                    println!("⚠️ Output is not valid JSON, skipping extraction");
-                }
-            }
-        }
+        run_task(task, &mut context)?
     }
 
+    Ok(())
+}
+
+pub fn run_task(task: Task, context: &mut HashMap<String, String>) -> anyhow::Result<()> {
+    println!("===> Running task {}", task.name);
+    // println!("{:?}", task);
+    println!("Context: {:?}", context);
+
+    let handlebars = Handlebars::new();
+    let rendered = handlebars.render_template(&task.command, &context)?;
+    println!("$ {}", rendered);
+
+    let output = Command::new("sh").arg("-c").arg(&rendered).output()?;
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    //let stderr = String::from_utf8_lossy(&output.stderr);
+    println!("{}", stdout);
+
+    add_to_context(&task, context, &stdout)?;
+
+    let mut is_ok = false;
+    if let Some(ok_match) = task.ok_match {
+        let context_match = if ok_match.context.is_empty() {
+            true
+        } else {
+            ok_match.context.iter().all(|(key, matcher)| {
+                context.get(key).map_or(false, |value| matcher.is_match(value))
+            })
+        };
+        let output_match = if ok_match.output.is_empty() {
+            true
+        } else {
+           ok_match.output.iter().all(|matcher| {
+               matcher.is_match(&stdout)
+           })
+        };
+        is_ok = context_match && output_match;
+    };
+
+    if is_ok && task.when_ok.is_some(){
+        run_task(*task.when_ok.unwrap(), context)?;
+    }
+
+    if !is_ok && task.when_err.is_some() {
+        run_task(*task.when_err.unwrap(), context)?;
+    }
+
+    Ok(())
+}
+
+fn add_to_context(task: &Task, context: &mut HashMap<String, String>, stdout: &str) -> anyhow::Result<()> {
+    if let Some(rules) = &task.extract {
+        if let Ok(json) = serde_json::from_str::<Value>(&stdout) {
+            for rule in rules {
+                let results = select(&json, &rule.field)?;
+                if let Some(val) = results.get(0) {
+                    let val_str = val.to_string().trim_matches('"').to_string();
+                    context.insert(rule.as_key.clone(), val_str.clone());
+                    println!("→ Extracted {} = {}", rule.as_key, val_str);
+                }
+            }
+        } else {
+            println!("⚠️ Output is not valid JSON, skipping extraction");
+        }
+    }
     Ok(())
 }
